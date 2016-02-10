b629161b30b0c9923cc1ddf7a61bcbee03fb6112
Your Name <you@example.com>
2016-02-10 14:58:35 +0400

    get access token

--- src/Facebook/MenuBundle/Controller/DefaultController.php
+++ src/Facebook/MenuBundle/Controller/DefaultController.php
@@ -9,13 +9,26 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-    	$facebook = new FacebookMenuController();
+    	if(!session_id()){
+            session_start();
+        }
 
+    	$facebook = new FacebookMenuController();
         $loginUrl = $facebook->getLoginUrl();
         $params = [
             "Title" => "Connexion à Facebook",
             "loginUrl" => $loginUrl
         ];
         return $this->render('FacebookMenuBundle:Default:login.html.twig', $params);
+    }
+
+    public function showMenuAction()
+    {
+    	if( isset($_SESSION["facebook_access_token"]) && !empty($_SESSION["facebook_access_token"]) ){
+    		return $this->render('FacebookMenuBundle:Default:menuToday.html.twig');
+    	}else{
+    		return $this->redirect($this->generateUrl("facebook_menu_homepage"));
+    	}
+        
     }
 }
--- src/Facebook/MenuBundle/Controller/FacebookMenuController.php
+++ src/Facebook/MenuBundle/Controller/FacebookMenuController.php
@@ -11,7 +11,8 @@ use Facebook\Facebook;
 class FacebookMenuController extends Controller {
 
     const app_id = "142914352759046";
-    const app_secret = "3f6ceb59023bf6323c44dae75e2a00fe"; 
+    const app_secret = "3f6ceb59023bf6323c44dae75e2a00fe";
+    const base_url = 'http://localhost:8000';
     
   
     public function getLoginUrl() {
@@ -23,9 +24,44 @@ class FacebookMenuController extends Controller {
         
         $helper = $fb->getRedirectLoginHelper();
         $permissions = ['email', 'user_likes'];
-        $loginUrl = $helper->getLoginUrl('http://localhost:8000/app_dev.php/callbackFb', $permissions);
+        $loginUrl = $helper->getLoginUrl( self::base_url . '/app_dev.php/getToken', $permissions);
         return $loginUrl;
     }
-    
+
+    public function getTokenAction() {
+        if(!session_id()){
+            session_start();
+        }
+        $fb = new Facebook([
+            "app_id" => self::app_id,
+            "app_secret" => self::app_secret,
+            'default_graph_version' => 'v2.5',
+            //'default_access_token' => '{access-token}', // optional
+        ]);
+
+        // Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
+        $helper = $fb->getRedirectLoginHelper();
+        //   $helper = $fb->getJavaScriptHelper();
+        //   $helper = $fb->getCanvasHelper();
+        //   $helper = $fb->getPageTabHelper();
+
+        //Get token
+        try {
+            $accessToken = $helper->getAccessToken();
+        } catch (Facebook\Exceptions\FacebookResponseException $e) {
+            echo 'Graph returned an error: ' . $e->getMessage();
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        }
+
+        //Save token in a session variable
+        if (isset($accessToken)) {
+            $_SESSION['facebook_access_token'] = (string) $accessToken;
+            return $this->redirect($this->generateUrl("facebook_menu_show"));
+        }else{
+            return $this->redirect($this->generateUrl("facebook_menu_homepage"));
+        }
+        
+    }
 
 }
