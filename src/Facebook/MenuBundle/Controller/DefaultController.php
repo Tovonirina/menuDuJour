16a4513f9e493e51915ec5675c8de49a76d08d05
Your Name <you@example.com>
2016-02-11 17:49:38 +0400

    get and show all menu

--- src/Facebook/MenuBundle/Controller/DefaultController.php
+++ src/Facebook/MenuBundle/Controller/DefaultController.php
@@ -5,14 +5,14 @@ namespace Facebook\MenuBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Facebook\MenuBundle\Controller\FacebookMenuController;
 
+if(!session_id()){
+    session_start();
+}
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-    	if(!session_id()){
-            session_start();
-        }
-
     	$facebook = new FacebookMenuController();
         $loginUrl = $facebook->getLoginUrl();
         $params = [
@@ -24,8 +24,19 @@ class DefaultController extends Controller
 
     public function showMenuAction()
     {
+    	$facebook = new FacebookMenuController();
+
     	if( isset($_SESSION["facebook_access_token"]) && !empty($_SESSION["facebook_access_token"]) ){
-    		return $this->render('FacebookMenuBundle:Default:menuToday.html.twig');
+
+    		$allMenu = $facebook->getAllMenuContent();
+            
+            $params = [
+                "lilotregal" => $allMenu['lilotregal'],
+                "regalducircuit" => $allMenu['regalducircuit']
+            ];
+
+    		return $this->render('FacebookMenuBundle:Default:menuToday.html.twig' , $params );
+
     	}else{
     		return $this->redirect($this->generateUrl("facebook_menu_homepage"));
     	}
--- src/Facebook/MenuBundle/Controller/FacebookMenuController.php
+++ src/Facebook/MenuBundle/Controller/FacebookMenuController.php
@@ -5,39 +5,36 @@ namespace Facebook\MenuBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Facebook\Facebook;
 
+
+if(!session_id()){
+    session_start();
+}
+
 /**
  * Controleur gérant les appels au GraphAPI de facebook
  */
 class FacebookMenuController extends Controller {
 
-    const app_id = "142914352759046";
-    const app_secret = "3f6ceb59023bf6323c44dae75e2a00fe";
+
     const base_url = 'http://localhost:8000';
-    
-  
+
+    const fb_parameters = array(
+        "app_id" => "142914352759046",
+        "app_secret" => "3f6ceb59023bf6323c44dae75e2a00fe",
+        "default_graph_version" => 'v2.5'
+    );
+
     public function getLoginUrl() {
-        $fb_params = [
-            "app_id" => self::app_id,
-            "app_secret" => self::app_secret
-        ];
-        $fb = new Facebook($fb_params);
         
+        $fb = new Facebook(self::fb_parameters);        
         $helper = $fb->getRedirectLoginHelper();
-        $permissions = ['email', 'user_likes'];
-        $loginUrl = $helper->getLoginUrl( self::base_url . '/app_dev.php/getToken', $permissions);
+        $loginUrl = $helper->getLoginUrl( self::base_url . '/app_dev.php/getToken', ['email']);
         return $loginUrl;
     }
 
-    public function getTokenAction() {
-        if(!session_id()){
-            session_start();
-        }
-        $fb = new Facebook([
-            "app_id" => self::app_id,
-            "app_secret" => self::app_secret,
-            'default_graph_version' => 'v2.5',
-            //'default_access_token' => '{access-token}', // optional
-        ]);
+    public function getTokenAction() {        
+
+        $fb = new Facebook(self::fb_parameters);
 
         // Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
         $helper = $fb->getRedirectLoginHelper();
@@ -54,14 +51,92 @@ class FacebookMenuController extends Controller {
             echo 'Facebook SDK returned an error: ' . $e->getMessage();
         }
 
-        //Save token in a session variable
+        //Save token in a session variable then redirect
         if (isset($accessToken)) {
             $_SESSION['facebook_access_token'] = (string) $accessToken;
             return $this->redirect($this->generateUrl("facebook_menu_show"));
         }else{
             return $this->redirect($this->generateUrl("facebook_menu_homepage"));
         }
+
+    }
+
+    public function getMenuList( $posts , $title_pattern , $msg_separator){
+        $latest_menus = array();
+
+        $day = array("dimanche","lundi","mardi","mercredi","jeudi","vendredi","samedi"); 
+        $month = array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"); 
+        $date = explode('|', date("w|d|n|Y"));
+        $timestamp = time () ;
+        $date = explode('|', date( "w|d|n|Y", $timestamp ));
+        $today = $day[$date[0]] . ' ' . $date[1] . ' ' . $month[$date[2]-1] ;
+
+        foreach( $posts as $key => $post){
+
+            if (stripos($post->message, $title_pattern ) !== false) {
+                $exploded_message = explode($msg_separator, $post->message, 2);
+                $title = $exploded_message[0];
+                $list_menu = nl2br( $exploded_message[1] );
+                $list_menu = preg_replace('/<br \/>/', '', $list_menu, 1);
+
+                $date_on_menu = $this->wd_remove_accents( trim( str_replace( $title_pattern , "", $title) ) );
+                
+                
+                $wanted_fav_dish = array("Poulet à la crème", "Rougail saucisse");
+                $raw_menu = $this->wd_remove_accents($list_menu);
+                $founded_fav_dish = array();
+                foreach( $wanted_fav_dish as $fav_dish){ 
+                    $raw_fav_dish = $this->wd_remove_accents($fav_dish) ;                 
+                    if (stripos($raw_menu, $raw_fav_dish) !== false){
+                        array_push($founded_fav_dish, $fav_dish);
+                    }
+                }
+                
+                $content_array = [
+                    "title" => $title,
+                    "list_menu" => $list_menu,
+                    "full_picture" => $post->full_picture,
+                    "is_today_menu" => ($date_on_menu == $today),
+                    "founded_fav_dish" => $founded_fav_dish
+                ];
+                array_push($latest_menus, $content_array);
+            }
+            if(count($latest_menus) >= 3){
+                break;
+            }
+        }
+
+        return $latest_menus;
+    }
+
+    public function getAllMenuContent(){
+        $fb = new Facebook(self::fb_parameters);
+
+        $accessToken = $_SESSION["facebook_access_token"];  
         
+        $request1 = $fb->get('/lilotregal/posts?fields=message,full_picture', $accessToken);
+        $rawMenu1 = json_decode($request1->getBody()) -> data;
+
+        $request2 = $fb->get('/649823778452363/posts?fields=message,full_picture', $accessToken);
+        $rawMenu2 = json_decode($request2->getBody()) -> data;         
+
+        $all_menu=[
+            "lilotregal" => $this -> getMenuList( $rawMenu1 , "Menu du" , ":"),
+            "regalducircuit" => $this -> getMenuList( $rawMenu2 , "Repas du jour" , "\n")
+        ];
+
+        return $all_menu; 
+    }
+
+    // Remove accents [www.weirdog.com] 
+    function wd_remove_accents($str, $charset = 'utf-8') {
+        $str = htmlentities($str, ENT_NOQUOTES, $charset);
+
+        $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+        $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
+        $str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
+
+        return $str;
     }
 
 }
